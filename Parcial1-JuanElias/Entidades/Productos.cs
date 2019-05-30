12b95576dcc3559a27ea1ff59abb9ac10e9b12a4
Juan Rosario <juanelias18@hotmail.com>
2019-05-29 23:52:52 -0400

    Entidad Productos creada.

---
+++ Parcial1-JuanElias/Entidades/Productos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial1_JuanElias.Entidades
+{
+    public class Productos
+    {
+        [Key]
+        public int ProductoID { get; set; }
+        public string Descripcion { get; set; }
+        public int Existencia { get; set; }
+        public int Costo { get; set; }
+        public int ValorInventario { get; set; }
+
+        public Productos()
+        {
+            ProductoID = 0;
+            Descripcion = string.Empty;
+            Existencia = 0;
+            Costo = 0;
+            ValorInventario = 0;
+        }
+    }
+}
