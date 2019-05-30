d23445a6de278600fdbdbdd82ec2048bce260609
Juan Rosario <juanelias18@hotmail.com>
2019-05-29 23:57:39 -0400

    DAL Creado. Contexto completado.

---
+++ Parcial1-JuanElias/DAL/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using Parcial1_JuanElias.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial1_JuanElias.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Productos> productos { get; set; }
+
+        public Contexto() : base("ConStr") { }
+    }
+}
