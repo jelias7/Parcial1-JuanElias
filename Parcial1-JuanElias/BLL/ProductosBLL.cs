09338af083a53816d33ed50e863c516287a1c81b
Juan Rosario <juanelias18@hotmail.com>
2019-05-29 23:40:31 -0400

    Comenzando ProductosBLL. Metodo Guardar completado.

---
+++ Parcial1-JuanElias/BLL/ProductosBLL.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial1_JuanElias.BLL
+{
+    public class ProductosBLL
+    {
+        public static bool Guardar(Productos productos)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+            try
+            {
+                if (db.productos.Add(productos) != null)
+                    paso = db.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return paso;
+        }
+    }
+}
