5c26ee0d1d253768628686f60fe6f60260b8edf4
Juan Rosario <juanelias18@hotmail.com>
2019-06-02 14:32:36 -0400

    Entidad Inventario creada.

---
+++ Parcial1-JuanElias/Entidades/Inventario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial1_JuanElias.Entidades
+{
+    public class Inventario
+    {
+        public int id { get; set; }
+        public float total { get; set; }
+
+        public Inventario()
+        {
+            id = 0;
+            total = 0;
+        }
+    }
+}
